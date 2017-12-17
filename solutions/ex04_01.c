cbb7e73c73a2e78c6d788bb305bfddfff19d852c
Ralph Recto <recto.ralph@gmail.com>
2017-12-17 21:38:29 +0000

    ex04_01 (tee): writing stdin to stdout

--- solutions/ex04_01.c
+++ solutions/ex04_01.c
@@ -1,5 +1,19 @@
+/**
+ * Implementation of tee using syscalls.
+ */
+
 #include "tlpi_hdr.h"
 
 int main(int argc, char* args[]) {
-	printf("hello, world\n");	
+  int max_read = 128;
+  char buf[max_read];
+  int read_res;
+
+  while ((read_res = read(STDIN_FILENO, buf, max_read)) > 0) {
+    write(STDOUT_FILENO, buf, read_res);
+  }
+
+  if (read_res < 0) {
+    errExit("reading from stdin");
+  }
 }
